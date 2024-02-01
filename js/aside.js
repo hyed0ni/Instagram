5fc7b40f78f7ae6aaa6c1ac5cf1d5b6da8146741
hyed0ni <layssing10955@gmail.com>
2024-02-01 14:53:16 +0900

    검색창 완료

--- js/aside.js
+++ js/aside.js
@@ -7,6 +7,19 @@ document.addEventListener("DOMContentLoaded", function() {
             document.querySelector(".aside .detail-window").style.display = "flex";
         });
 
+        // 검색 아이콘 -> 검색 내용 삭제 아이콘
+        document.querySelector(".aside .search-window .input .text").addEventListener("click", function() {
+            document.querySelector(".aside .search-window .input > div > div:first-child").style.display = "none";
+            document.querySelector(".aside .search-window .input > div > div:last-child").style.display = "block";
+        });
+
+        // 최근 검색 항목 -> 검색 목록
+        document.querySelector(".aside .search-window .input .text").addEventListener("click", function() {
+            document.querySelector(".aside .search-window hr").style.display = "none";
+            document.querySelector(".aside .recently").style.display = "none";
+            document.querySelector(".aside .search-list").style.display = "block";
+        });
+
     },300)
 
 })
